9db46455700e1b7f0581d40daf06b9f17b6e4acd
kidekho88-beep <kidekho88@gmail.com>
2026-03-02 19:43:47 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -130,7 +130,7 @@ def numbers_keyboard(country, selected):
         copy_val = "+" + clean
 
         # 👇 Hair space ব্যবহার (almost invisible)
-        button_text = f"{flag}{clean}"
+        button_text = clean"
 
         rows.append([
             InlineKeyboardButton(
